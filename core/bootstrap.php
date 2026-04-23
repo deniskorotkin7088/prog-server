e58dc45dc838230a414a517e91b6d57030b226cb
honor <deniskorotkin7088@gmail.com>
2026-04-23 21:12:21 +0700

    fix bd

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -8,15 +8,27 @@ require_once __DIR__ . '/../vendor/autoload.php';
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
 {
-   $settings = [];
-   foreach (scandir(__DIR__ . $path) as $file) {
-       $name = explode('.', $file)[0];
-       if (!empty($name)) {
-           $settings[$name] = include __DIR__ . "$path/$file";
-       }
-   }
-   return $settings;
+    $settings = [];
+    foreach (scandir(__DIR__ . $path) as $file) {
+        $name = explode('.', $file)[0];
+        if (!empty($name)) {
+            $settings[$name] = include __DIR__ . "$path/$file";
+        }
+    }
+    return $settings;
 }
+
+// ПОДКЛЮЧАЕМ МАРШРУТЫ (ДО return!)
 require_once __DIR__ . '/../routes/web.php';
 
-return new Src\Application(new Src\Settings(getConfigs()));
+// СОЗДАЁМ ПРИЛОЖЕНИЕ
+$app = new Src\Application(new Src\Settings(getConfigs()));
+
+// ГЛОБАЛЬНАЯ ФУНКЦИЯ
+function app() {
+    global $app;
+    return $app;
+}
+
+// ВОЗВРАЩАЕМ ПРИЛОЖЕНИЕ
+return $app;
